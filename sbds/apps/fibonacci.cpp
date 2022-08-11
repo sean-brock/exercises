b69414e3401176c97dd9fab839b881580477635e
sean-brock <4583759+sean-brock@users.noreply.github.com>
2022-08-10 20:25:02 -0500

    Added recursive fib

--- sbds/apps/fibonacci.cpp
+++ sbds/apps/fibonacci.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
+#include <memory>
 
 /*
   fib (n) 0 1 1 2 3 5 8 13
   n       0 1 2 3 4 5 6 7
 
-  fib(n) = 
+  fib(n) =
     n if n <= 1
     fib(n-2) + fib(n-1)
 */
-int fib_recursive(int n) { return 0; }
+int fib_recursive(int *cache, int n) {
+  if (n <= 1) {
+    cache[n] = n;
+    return n;
+  }
+  if (cache[n - 2] < 0)
+    cache[n - 2] = fib_recursive(cache, n - 2);
+  if (cache[n - 1] < 0)
+    cache[n - 1] = fib_recursive(cache, n - 1);
 
-int fib(int n) { 
+  return cache[n-1] + cache[n-2];
+}
+
+int fib(int n) {
   if (n <= 1)
     return n;
 
-  int cache[] = {0,1};
+  int cache[] = {0, 1};
   int tmp = 0;
   for (int i = 2; i <= n; i++) {
     tmp = cache[0] + cache[1];
     cache[0] = cache[1];
     cache[1] = tmp;
   }
-  return cache[1]; 
+  return cache[1];
 }
 
 int main() {
-  std::cout << fib(6) << "\n" << fib_recursive(6) << std::endl;
+  constexpr int n = 20;
+
+  auto cache = std::make_unique<int[]>(n);
+  for (int i = 0; i < n; i++) {
+    cache[i] = -1;
+  }
+
+  std::cout << fib(n) << "\n" << fib_recursive(cache.get(), n) << std::endl;
+
+  int x = 1;
+  std::cin >> x;
+  int arr[x];
+
+  std::cout << x <<std::endl;
+
   return 0;
 }
